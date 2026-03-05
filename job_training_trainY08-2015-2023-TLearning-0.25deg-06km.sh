7c39dd4eef5c11fc3aff973ae05723b26e548c81
Bo Huang <bo.huang@noaa.gov>
2026-03-05 11:19:22 -0800

    Modify data and model yaml to use gfs-hrrr data

--- job_training_trainY08-2015-2023-TLearning-0.25deg-06km.sh
+++ job_training_trainY08-2015-2023-TLearning-0.25deg-06km.sh
@@ -19,4 +19,4 @@ module load conda
 conda activate anemoi
 export MPLBACKEND=Agg
 
-srun --jobid $SLURM_JOB_ID /global/u1/b/bhuang95/sorc/fromTim/anemoi/V20250603/anemoi-house//slurm2ddp.sh anemoi-training train --config-name=config_trainY01-2018-2018-TLearning-0.25deg-06km
+srun --jobid $SLURM_JOB_ID /global/u1/b/bhuang95/sorc/fromTim/anemoi/V20250603/anemoi-house//slurm2ddp.sh anemoi-training train --config-name=config_trainY08-2015-2023-TLearning-0.25deg-06km
