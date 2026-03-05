0511f6530d8d141bde13f71289acea4fd0818f65
Bo Huang <bo.huang@noaa.gov>
2026-03-05 15:42:05 -0800

    Add log files for check

---
+++ job_training_trainY08-2015-2023-TLearning-0.25deg-06km-Step-C.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH -J trainY01-2018-2018-T1
+#SBATCH -o training.trainY01-2018-2018-TLearning-0.25deg-Step-C.%j.out
+#SBATCH -e training.trainY01-2018-2018-TLearning-0.25deg-Step-C.%j.out
+#SBATCH --nodes=4
+#SBATCH --tasks-per-node=4
+#SBATCH --gpus-per-node=4
+#SBATCH --cpus-per-task=16
+#SBATCH --qos=debug
+#SBATCH --account=m4718
+#SBATCH --constraint=gpu&hbm80g
+#SBATCH -t 00:30:00
+
+# salloc --nodes 4 --qos interactive --time 00:30:00 --constraint gpu --account m4718
+# srun --jobid $SLURM_JOB_ID --tasks-per-node=4 --gpus-per-node=4 --cpus-per-task=16 /global/u1/b/bhuang95/sorc/fromTim/anemoi/V20250603/anemoi-house/slurm2ddp.sh anemoi-training train --config-name=config
+
+module load conda
+conda activate anemoi
+export MPLBACKEND=Agg
+
+srun --jobid $SLURM_JOB_ID /global/u1/b/bhuang95/sorc/fromTim/anemoi/V20250603/anemoi-house//slurm2ddp.sh anemoi-training train --config-name=config_trainY08-2015-2023-TLearning-0.25deg-06km-Step-C
